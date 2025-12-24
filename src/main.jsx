1b39d6d1d77c14917b3ff73c586a464f075a21c7
Sarthak Mahapatra <sarthakmahapatra303@gmail.com>
2025-12-25 02:51:05 +0530

    Gallery-page-added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Home from './pages/Home';
+import Gallery from './pages/Gallery';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="gallery" element={<Gallery />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 }
